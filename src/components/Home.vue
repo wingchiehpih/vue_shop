7d7f212584c8ee0c61ab770c0e9d2d44fb234a84
wingchiehpih <wingchiehpih@gmail.com>
2021-07-05 15:24:37 +0800

    完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Home组件
+    <el-button type="Infor" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 需要添加后缀命名
+// 在全局引入抽离的elementui配置项
+import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import "./assets/css/global.css"
+
+import axios from 'axios'
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
